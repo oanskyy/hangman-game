d5b13e3bc59086145993d57fa3c4e39529c41c76
codessbabe <oana.anghelachi@gmail.com>
2020-07-02 08:24:23 +0200

    Display 'words function'

--- script.js
+++ script.js
@@ -18,7 +18,23 @@ const wrongLetters = [];
 
 // Show hidden word
 function displayWord() { 
-  
+  wordEl.innerHTML = `
+    ${selectedWord
+      .split('')
+      .map(letter => 
+        `<span class="letter">
+          ${correctLetters.includes(letter) ? letter : ''}
+        </span>`)
+      .join('')
+    } 
+  `;
+
+  const innerWord = wordEl.innerText.replace(/\n/g, '');
+    if(innerWord === selectedWord) { 
+      finalMessage.innerText = 'Congratulations! You won!'; 
+      popup.style.display = 'flex'; 
+    }
+
 }
 
 displayWord();
