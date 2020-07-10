07ff511049717b2a89c228e19028b90b7cf51892
codessbabe <oana.anghelachi@gmail.com>
2020-07-10 11:35:11 +0200

    Create letter-press event-handler

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const playAgainBtn = document.getElementById('play-again');
 const popup = document.getElementById('popup-container'); 
 const notification = dcoument.getElementById('notification-container'); 
 const finalMessage = document.getElementById('final-message'); 
-
 const figureParts = document.querySelectorAll('.figure-part');
 
 const words = ['application', 'programming', 'interface', 'golum']; 
@@ -34,7 +33,42 @@ function displayWord() {
       finalMessage.innerText = 'Congratulations! You won!'; 
       popup.style.display = 'flex'; 
     }
-
 }
+
+// Update the wrong letters 
+function updateWrongLettersEl() { 
+  console.log('Update wrong');
+}
+
+// Show notification 
+function showNotification() { 
+  notification.classList.add('show'); 
+  setTimeout(() => { 
+    notification.classList.remove('show');
+  }, 2000); 
+}
+
+// Keydown letter press 
+window.addEventListener('keydown', e => { 
+  if (e.keyCode >= 65 && e.keyCode <= 90) { 
+    const letter = e.key;
+
+    if (selectedWord.includes(letter)) { 
+      if (!correctLetters.includes(letter)) { 
+        correctLetters.push(letter); 
+        displayWord();
+      } else { 
+        showNotification();
+      }
+    } else {  
+      if(!wrongLetters.includes(letter)) { 
+        wrongLetters.push(letter); 
+        updateWrongLettersEl(); 
+      } else { 
+        showNotification();
+      }
+    }
+  }
+});
 
 displayWord();
