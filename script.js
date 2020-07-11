c1db0b124130df6e7caad1b88e843b25f6834369
codessbabe <oana.anghelachi@gmail.com>
2020-07-11 23:19:39 +0200

    Add wrong letters and play-button

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // Grab DOM elements that we need 
 const wordEl = document.getElementById('word'); 
 const wrongLettersEl = document.getElementById('wrong-letters'); 
-const playAgainBtn = document.getElementById('play-again'); 
+const playAgainBtn = document.getElementById('play-button'); 
 const popup = document.getElementById('popup-container'); 
 const notification = dcoument.getElementById('notification-container'); 
 const finalMessage = document.getElementById('final-message'); 
@@ -37,8 +37,29 @@ function displayWord() {
 
 // Update the wrong letters 
 function updateWrongLettersEl() { 
-  console.log('Update wrong');
+  // Display wrong letters
+  wrongLettersEl.innerHTML = `
+  ${wrongLetters.length > 0 '<p>Wrong</p>' : ''}
+  ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+  `;
+
+  // Display parts
+  figureParts.forEach((part, index) => { 
+    const errors = wrongLetters.length; 
+    if (index < errors) { 
+      part.style.display = 'block';
+    } else { 
+      part.style.display = 'none';
+    }
+  })
+
+  // Check if lost 
+  if(wrongLetters.length === figureParts.length) { 
+    finalMessage.innerText = 'Unfortunately you lost :( '; 
+    popup.style.display = 'flex';
+  }
 }
+
 
 // Show notification 
 function showNotification() { 
@@ -70,5 +91,17 @@ window.addEventListener('keydown', e => {
     }
   }
 });
+
+// Restart game & play again 
+playAgainBtn.addEventListener('click', () => { 
+  // Empty arrays 
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)]; 
+  displayWord();
+  updateWrongLettersEl();
+  popup.style.display = 'none';
+})
 
 displayWord();
